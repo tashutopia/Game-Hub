30d9a16c90f8462528dd7d1754236ff2f4132a1b
tashutopia <tashutopia@gmail.com>
2024-01-11 22:39:43 -0500

    Finished list functionality, updated README

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,19 +1,17 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let items = [
-    "New York",
-    "San Francisco",
-    "Los Angeles",
-    "Chicago",
-    "Houston",
-  ];
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
 
+function ListGroup({ items, heading, onSelectItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>There are no items in the list</p>}
       <ul className="list-group">
         {items.map((item, index) => (
@@ -26,6 +24,7 @@ function ListGroup() {
             key={item}
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
           >
             {item}
