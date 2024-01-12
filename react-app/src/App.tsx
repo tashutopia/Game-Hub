ec22f0f894549b3de945eb32eac7e870b1d09b45
tashutopia <tashutopia@gmail.com>
2024-01-12 00:16:48 -0500

    Added Alert and Button

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,3 +1,5 @@
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
@@ -15,6 +17,12 @@ function App() {
 
   return (
     <div>
+      <Button color="primary" onClick={() => console.log("Clicked")}>
+        My Button
+      </Button>
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
       <ListGroup
         items={items}
         heading="Cities"
